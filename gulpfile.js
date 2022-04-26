3a4ae62cfce3f21013ae6f811e02150bd9de4308
Muhannad Al Dardiri <73785249+Mohannad-AlDardiri@users.noreply.github.com>
2022-04-26 16:38:14 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let uglify = require('gulp-uglify');
 // Task to minify HTML
 gulp.task('minify-html', function() {
 return gulp.src('src/origin.html')
-.pipe(htmlmin())
+.pipe(htmlmin({ collapseWhitespace: true }))
 .pipe(gulp.dest('dist'));
 // I moved the file to the root to easily deploy it
 });
